92a7adb3e055ac4dceefa36e248bd6acd03e2c15
Mark Wheeler <mark@markwheeler.net>
2020-05-24 23:20:17 -0700

    lots

--- slowfo.lua
+++ slowfo.lua
@@ -4,15 +4,20 @@
 -- Generate slow sine waves
 -- over MIDI.
 --
+-- E2 : Low freq
+-- E3 : High freq
+-- K2 : Reset phase
+--
 
-local UI = require "ui"
+local ControlSpec = require "controlspec"
 
 local SCREEN_FRAMERATE = 15
-local screen_refresh_clock
 local screen_dirty = true
 
 local NUM_LFOS = 8
 local LFO_RESOLUTION = 128 -- MIDI CC resolution
+local LFO_MIN_FREQ = 1 / 60 / 60 / 24 -- 24hrs
+local LFO_MAX_FREQ = 0.5 -- 2 secs
 local lfo_freqs = {}
 local lfo_progress = {}
 local lfo_values = {}
@@ -21,47 +26,97 @@ local midi_out_device
 local midi_out_channel
 
 
--- Clock
-local function tick(time)
+local specs = {}
+specs.FREQ_L = ControlSpec.new(LFO_MIN_FREQ, LFO_MAX_FREQ, "exp", 0, 0.001, "Hz")
+specs.FREQ_R = ControlSpec.new(LFO_MIN_FREQ, LFO_MAX_FREQ, "exp", 0, 0.1, "Hz")
+
+
+local function format_freq_to_time(param)
+  local secs = 1 / param:get()
+  local out_string
+  if secs > 3600 then
+    out_string = util.round(secs / 60 / 60, 0.1) .. " hrs"
+  elseif secs > 120 then
+    out_string = util.round(secs / 60, 0.1) .. " mins"
+  elseif secs > 60 then
+    out_string = util.round(secs) .. " secs"
+  else
+    out_string = util.round(secs, 0.1) .. " secs"
+  end
+  return out_string
+end
+
+local function reset_phase()
+  for i = 1, NUM_LFOS do
+    lfo_progress[i] = 0
+  end
+end
+
+local function update_freqs()
+  for i = 1, NUM_LFOS do
+    lfo_freqs[i] = util.linlin(1, NUM_LFOS, params:get("freq_l"), params:get("freq_r"), i)
+  end
+end
+
+
+-- Clock callbacks
+
+local function lfo_tick()
   while true do
+    local time = util.time()
     for i = 1, NUM_LFOS do
-      lfo_progress[i] = lfo_progress[i] + 2 * math.pi * (1 / lfo_freqs[i]) --TODO?
-      local value = math.sin(lfo_progress[i])
+      -- TODO switch method here to use progress properly
+      lfo_progress[i] = time * 2 * math.pi * lfo_freqs[i]
+      local value = util.linlin(-1, 1, 0, LFO_RESOLUTION - 1, math.sin(lfo_progress[i]))
       if value ~= lfo_values[i] then
         lfo_values[i] = value
-        print(i, "Tick", value) --TODO only fire if value has changed
+        midi_out_device:cc(i, value, midi_out_channel)
+        -- print(i, "Tick", value) --TODO
       end
     end
     screen_dirty = true
-    clock.sleep(time)
+
+    clock.sleep(1 / LFO_MAX_FREQ / LFO_RESOLUTION)
+  end
+end
+
+local function screen_tick()
+  while true do
+    -- print("screen tick", util.time())
+    if screen_dirty then
+      screen_dirty = false
+      redraw()
+    end
+    clock.sleep(1 / SCREEN_FRAMERATE)
   end
 end
 
 
 -- Encoder input
 function enc(n, delta)
-
+  if n == 2 then
+    params:delta("freq_l", delta * -0.1)
+  elseif n == 3 then
+    params:delta("freq_r", delta * -0.1)
+  end
 end
 
 -- Key input
 function key(n, z)
-
+  if z == 1 then
+    if n == 2 then
+      reset_phase()
+    end
+  end
 end
 
 
 function init()
   midi_out_device = midi.connect(1)
 
-  -- TODO update to clock
-  local screen_refresh_metro = metro.init()
-  screen_refresh_metro.event = function()
-    if screen_dirty then
-      screen_dirty = false
-      redraw()
-    end
-  end
-
   -- Add params
+  
+  params:add_separator()
 
   params:add {
     type = "number",
@@ -88,28 +143,66 @@ function init()
     end
   }
 
+  params:add_separator("LFOs")
+
+  params:add {
+    type = "control",
+    id = "freq_l",
+    name = "Left Freq",
+    controlspec = specs.FREQ_L,
+    formatter = format_freq_to_time,
+    action = function(value)
+      update_freqs()
+    end
+  }
+
+  params:add {
+    type = "control",
+    id = "freq_r",
+    name = "Right Freq",
+    controlspec = specs.FREQ_R,
+    formatter = format_freq_to_time,
+    action = function(value)
+      update_freqs()
+    end
+  }
+
   midi_out_channel = params:get("midi_out_channel")
 
-  for i = 1, NUM_LFOS do
-    lfo_freqs[i] = 0.5
-    lfo_progress[i] = 0
-  end
-
-  local lfo_max_freq = 0.5 --TODO controlspec
-  clock.run(i, tick, 1 / lfo_max_freq / LFO_RESOLUTION)
-
+  reset_phase()
+  update_freqs()
+  
+  clock.run(lfo_tick)
+  clock.run(screen_tick)
 end
 
 
 function redraw()
   screen.clear()
 
-  screen.move(10, 10)
-  screen.text("Slowfo 1" .. lfo_values[1])
-  screen.level(15)
-  screen.fill()
+  local BAR_W, BAR_H = 1, 40
+  local MARGIN_H, MARGIN_V = 6, 6
+  local gutter = (128 - MARGIN_H * 2 - BAR_W * NUM_LFOS) / (NUM_LFOS - 1)
 
-  -- TODO draw some sliders/rects
+  -- Draw bars
+  for i = 1, NUM_LFOS do
+    local row_x = MARGIN_H + (gutter + BAR_W) * (i - 1)
+    screen.rect(row_x, MARGIN_V, BAR_W, BAR_H)
+    screen.level(1)
+    screen.fill()
+    local filled_height = util.linlin(0, LFO_RESOLUTION - 1, 0, BAR_H, lfo_values[i])
+    screen.rect(row_x, MARGIN_V + BAR_H - filled_height, BAR_W, filled_height)
+    screen.level(15)
+    screen.fill()
+  end
+
+  -- Draw text
+  screen.level(3)
+  screen.move(MARGIN_H, 64 - 5)
+  screen.text("\u{25C0} " .. params:string("freq_l"))
+  screen.move(128 - MARGIN_H, 64 - 5)
+  screen.text_right(params:string("freq_r") .. " \u{25B6}")
+  screen.fill()
 
   screen.update()
 end
